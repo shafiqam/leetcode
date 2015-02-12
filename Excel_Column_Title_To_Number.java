68c9bed0a0d86a3683df73811d446f34849d72cd
Shafiq Mohammed <shafiqali.md@hotmail.com>
2015-02-11 22:16:39 -0500

    #7 - convert excel column title to a number

--- Excel_Column_Title_To_Number.java
+++ Excel_Column_Title_To_Number.java
@@ -11,7 +11,8 @@ public class Excel_Column_Title_To_Number {
             // Multiply the current value of col_num by 26, and add the value of (c - 'A' + 1) or (c - 64) to the
             // string. I use the former one since it is easy to understand. We subtract the ASCII value of A from the
             // current character ASCII value and add one to the sum. 
-            // Say we convert B to print 2, we need to add 1 more to the result of (char - 'A') since ASCII('A') => 65.
+            // Say we convert B to print 2, we need to add 1 more to the result of (char - 'A') since 'A' corresponds
+            // to 1 and (char-'A') is removing 1 from the result already.
             col_num *= 26;
             col_num += c - 'A' + 1;
             //col_num += ((int) c) - 64;
