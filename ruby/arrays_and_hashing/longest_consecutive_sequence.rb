38f5f3933c2e97e4dc4698b94ce3f5d07e761f38
Shafiq Mohammed <shafiqsnss@gmail.com>
2023-07-01 15:30:56 -0400

    longest consecutive sequence

---
+++ ruby/arrays_and_hashing/longest_consecutive_sequence.rb
@@ -0,0 +1,19 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def longest_consecutive(nums)
+  # O(n), O(n)
+  set = Set.new(nums)
+  longest = 0
+  nums.each do |n|
+    # start of sequence
+    if !set.include? (n-1)
+      len = 1
+      while set.include? (n+1)
+        len += 1
+        n += 1
+      end
+      longest = len if len > longest
+    end
+  end
+  longest
+end
