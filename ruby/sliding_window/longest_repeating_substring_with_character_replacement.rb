268dc8974a0a2c68d8f56137b93c63b4168e730a
Shafiq Mohammed <shafiqsnss@gmail.com>
2023-07-06 18:13:27 -0400

    longest repeating substring with char replacement

---
+++ ruby/sliding_window/longest_repeating_substring_with_character_replacement.rb
@@ -0,0 +1,21 @@
+# @param {String} s
+# @param {Integer} k
+# @return {Integer}
+def character_replacement(s, k)
+  # O(n), O(26.n) ~ O(n)
+  hash = Hash.new(0) # alphabets count
+  longest = 0
+  l, r = 0, 0
+  while r < s.length
+    hash[s[r]] += 1
+    # window validity condition
+    # length of window - max of hash values <= k
+    if (r-l+1) - hash.values.max > k
+      hash[s[l]] -= 1 # keep accurate count of new window
+      l += 1
+    end
+    longest = [longest, r-l+1].max
+    r += 1
+  end
+  longest
+end
