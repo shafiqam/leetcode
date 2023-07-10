75cc8d166bc2022cc7b96c8a8032fe8197c1b951
Shafiq Mohammed <shafiqsnss@gmail.com>
2023-07-10 17:13:47 -0400

    merge two sorted linked lists

---
+++ ruby/linked_list/merge_two_sorted_lists.rb
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} list1
+# @param {ListNode} list2
+# @return {ListNode}
+def merge_two_lists(list1, list2)
+  # O(m+n), O(m+n)
+  dummy_node = ListNode.new
+  curr_node = dummy_node
+  while list1 && list2
+    if list1.val <= list2.val
+      curr_node.next = list1
+      list1 = list1.next
+    else
+      curr_node.next = list2
+      list2 = list2.next
+    end
+    curr_node = curr_node.next
+  end
+
+  # append to current_node if any one list has more values
+  curr_node.next = list1 ? list1 : list2
+
+  dummy_node.next
+end
