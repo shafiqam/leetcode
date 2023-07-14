682b28c112c70a90730f83f52ed53eb4589f5937
Shafiq Mohammed <shafiqsnss@gmail.com>
2023-07-14 19:07:51 -0400

    same binary tree

---
+++ ruby/trees/same_binary_tree.rb
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {Boolean}
+def is_same_tree(p, q)
+  # O(p+q), O(log[p,q].min)
+  if p.nil? && q.nil?
+    true
+  elsif p && q
+    p.val == q.val &&
+    is_same_tree(p.left, q.left) &&
+    is_same_tree(p.right, q.right)
+  else
+    # one exists and other does not
+    false
+  end
+end
