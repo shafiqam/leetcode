c6a517d57be8023d8af12877e5e9cf5c8076a82f
Shafiq Mohammed <shafiqsnss@gmail.com>
2023-07-19 20:36:45 -0400

    kth smallest element in bst

---
+++ ruby/trees/kth_smallest_element_in_bst.rb
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @param {Integer} k
+# @return {Integer}
+def kth_smallest(root, k)
+  # O(k), O(1)
+  return -1 if root.nil?
+
+  # iterative traversal
+  count = 0
+  stack = []
+  curr = root
+  while true
+    if curr
+      stack << curr
+      curr = curr.left
+    else
+      return -1 if stack.empty? # if k > size of tree
+
+      curr = stack.pop
+      count += 1
+      return curr.val if count == k
+
+      curr = curr.right
+    end
+  end
+end
