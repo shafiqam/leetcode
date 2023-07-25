58f0a7422de19b7c9debb4479260e4bd92059484
Shafiq Mohammed <shafiqsnss@gmail.com>
2023-07-24 21:02:59 -0400

    merge intervals

---
+++ ruby/intervals/merge_intervals.rb
@@ -0,0 +1,22 @@
+# @param {Integer[][]} intervals
+# @return {Integer[][]}
+def merge(intervals)
+  # O(nlogn), O(1)
+  intervals = intervals.sort_by { |i| i[0] }
+  # intervals.sort! # 2D arrays can be sorted as ssuch too
+
+  result = [intervals[0]]
+  for i in intervals[1..intervals.length-1]
+    last_intervals_end = result[-1][1]
+
+    # if the last_intervals_end is greater than curr intervals start, they overlap
+    if last_intervals_end >= i[0]
+      # update last intervals end to max of itself and curr interval
+      result[-1][1] = [last_intervals_end, i[1]].max
+    else
+      # no overlap, append the curr interval to list
+      result << i
+    end
+  end
+  result
+end
