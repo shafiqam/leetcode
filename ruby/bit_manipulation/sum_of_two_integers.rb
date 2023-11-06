95c48d450346e5b5c056df8a94d10b159f64264a
Shafiq Mohammed <shafiqsnss@gmail.com>
2023-11-05 22:32:51 -0500

    sum of two integers

---
+++ ruby/bit_manipulation/sum_of_two_integers.rb
@@ -0,0 +1,23 @@
+# @param {Integer} a
+# @param {Integer} b
+# @return {Integer}
+def get_sum(a, b)
+  # O(1), O(1)
+  ### logic
+  # while b != 0
+  #   carry = (a & b) << 1
+  #   a = a ^ b
+  #   b = carry
+  # end
+  # a
+
+  # avoid time limit
+  mask = 0xffffffff
+  while b != 0 # until no carries left
+    tmp = (a & b) << 1 # carries
+    a = (a ^ b) & mask # addition w/o carries
+    b = tmp & mask
+  end
+  a = ~(a ^ mask) if a > (mask >> 1)
+  a
+end
