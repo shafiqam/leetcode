bad5b5eb17c4e6d598e941c6429afddc6fba8ae9
Shafiq Mohammed <shafiqsnss@gmail.com>
2023-12-26 15:06:15 -0500

    graph valid tree

---
+++ ruby/graphs_advanced/graph_valid_tree.rb
@@ -0,0 +1,32 @@
+# @param n: An integer
+# @param edges: a list of undirected edges
+# @return: true if it's a valid tree, or false
+def valid_tree(n, edges)
+  # O(e+v), O(e+v)
+  return true if n.zero?
+
+  # create an adjacency list
+  adj_list = Hash.new { |h,k| h[k] = [] }
+  edges.each do |x, y|
+    # add both since its undirected
+    adj_list[x].append(y)
+    adj_list[y].append(x)
+  end
+
+  # visited is all the edges in the current dfs path
+  visited = Set.new
+
+  dfs = lambda do |i, prev|
+    return false if visited.include?(i) # loop exists
+
+    visited.add(i)
+    adj_list.each do |j|
+      next if j == prev # skip the false positive, since undirected graph
+      return false unless dfs.call(j, i)
+    end
+    return true
+  end
+
+  # dfs returns true and it is a connected graph (all nodes are connected, not disjoint)
+  dfs.call(0, -1) && n == visited.length
+end
